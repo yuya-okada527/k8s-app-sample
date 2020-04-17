f104b3b10413dc5864b2a47ea980fc5c2dc77114
yuya okada <yuya.okada527@gmail.com>
2020-04-17 20:18:27 +0900

    initial commit

---
+++ batch-app/src/test/java/com/example/k8s/sample/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k8s.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
